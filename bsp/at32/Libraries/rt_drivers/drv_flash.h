ca8af3e9233c5f12ad00045feaccc24f0ae3a639
qiyongzhong0 <917768104@qq.com>
2020-09-03 16:30:18 +0800

    add at32 drivers, files : drv_flash.h drv_flash_f4.c

---
+++ bsp/at32/Libraries/rt_drivers/drv_flash.h
@@ -0,0 +1,35 @@
+/*
+ * drv_flash_f4.c
+ *
+ * Change Logs:
+ * Date           Author            Notes
+ * 2020-09-03     qiyongzhong       first version
+ */
+
+#ifndef __DRV_FLASH_H__
+#define __DRV_FLASH_H__
+
+#include <rtthread.h>
+#include "rtdevice.h"
+#include <rthw.h>
+#include <drv_common.h>
+
+#if (AT32_FLASH_SIZE < (256*1024))
+#define AT32_FLASH_PAGE_SIZE    (1*1024)
+#else
+#define AT32_FLASH_PAGE_SIZE    (2*1024)
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int stm32_flash_read(rt_uint32_t addr, rt_uint8_t *buf, size_t size);
+int stm32_flash_write(rt_uint32_t addr, const rt_uint8_t *buf, size_t size);
+int stm32_flash_erase(rt_uint32_t addr, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* __DRV_FLASH_H__ */
---
+++ bsp/at32/Libraries/rt_drivers/drv_flash_f4.c
@@ -0,0 +1,183 @@
+/*
+ * drv_flash_f4.c
+ *
+ * Change Logs:
+ * Date           Author            Notes
+ * 2020-09-03     qiyongzhong       first version
+ */
+
+#include "board.h"
+ 
+#ifdef BSP_USING_ON_CHIP_FLASH
+#include "drv_flash.h"
+ 
+#if defined(PKG_USING_FAL)
+#include "fal.h"
+#endif
+ 
+ //#define DRV_DEBUG
+#define LOG_TAG                "drv.flash"
+#include <drv_log.h>
+
+ 
+ /**
+  * Read data from flash.
+  * @note This operation's units is byte.
+  *
+  * @param addr flash address
+  * @param buf buffer to store read data
+  * @param size read bytes size
+  *
+  * @return result
+  */
+int at32_flash_read(rt_uint32_t addr, rt_uint8_t *buf, size_t size)
+{
+    size_t i;
+
+    if ((addr + size) > AT32_FLASH_END_ADDRESS)
+    {
+        LOG_E("read outrange flash size! addr is (0x%p)", (void*)(addr + size));
+        return -RT_EINVAL;
+    }
+
+    for (i = 0; i < size; i++, buf++, addr++)
+    {
+        *buf = *(rt_uint8_t *) addr;
+    }
+
+    return size;
+}
+ 
+ /**
+  * Write data to flash.
+  * @note This operation's units is halfword.
+  * @note This operation must after erase. @see flash_erase.
+  *
+  * @param addr flash address
+  * @param buf the write data buffer
+  * @param size write bytes size
+  *
+  * @return result
+  */
+ 
+int at32_flash_write(rt_uint32_t addr, const rt_uint8_t *buf, size_t size)
+{
+    size_t i;
+    rt_err_t result = RT_EOK;
+    rt_uint16_t write_data = 0;
+
+    if ((addr + size) > AT32_FLASH_END_ADDRESS)
+    {
+        LOG_E("ERROR: write outrange flash size! addr is (0x%p)\n", (void*)(addr + size));
+        return -RT_EINVAL;
+    }
+
+    if(addr % 2 != 0)
+    {
+        LOG_E("write addr must be 2-byte alignment");
+        return -RT_EINVAL;
+    }
+
+    FLASH_Unlock();
+ 
+    for (i = 0; i<size; i++)
+    {
+        write_data = buf[1];
+        write_data <<= 8;
+        write_data += buf[0];
+        
+        if (FLASH_ProgramHalfWord(addr, write_data) != FLASH_PRC_DONE)
+         {
+             result = -RT_ERROR;
+             break;
+         }
+         if (*(rt_uint16_t*)addr != write_data)
+         {
+             LOG_E("ERROR: write data != read data\n");
+             result = -RT_ERROR;
+             break;
+         }
+         addr += 2;
+     }
+ 
+     FLASH_Lock();
+     
+     if (result != RT_EOK)
+     {
+         return result;
+     }
+ 
+     return size;
+ }
+ 
+/**
+ * Erase data on flash.
+ * @note This operation is irreversible.
+ * @note This operation's units is different which on many chips.
+ *
+ * @param addr flash address
+ * @param size erase bytes size
+ *
+ * @return result
+ */
+int at32_flash_erase(rt_uint32_t addr, size_t size)
+{
+    rt_err_t result = RT_EOK;
+    size_t pos = 0;
+
+    if ((addr + size) > AT32_FLASH_END_ADDRESS)
+    {
+        LOG_E("ERROR: erase outrange flash size! addr is (0x%p)\n", (void*)(addr + size));
+        return -RT_EINVAL;
+    }
+
+    FLASH_Unlock();
+
+    while (pos < size)
+    {
+        if (FLASH_ErasePage(addr) != FLASH_PRC_DONE)
+        {
+            result = -RT_ERROR;
+            break;
+        }
+        pos += AT32_FLASH_PAGE_SIZE;
+    }
+
+    FLASH_Lock();
+
+    if (result != RT_EOK)
+    {
+        return result;
+    }
+
+    LOG_D("erase done: addr (0x%p), size %d", (void*)addr, size);
+    return size;
+}
+ 
+#if defined(PKG_USING_FAL)
+ 
+ static int fal_flash_read(long offset, rt_uint8_t *buf, size_t size);
+ static int fal_flash_write(long offset, const rt_uint8_t *buf, size_t size);
+ static int fal_flash_erase(long offset, size_t size);
+ 
+ const struct fal_flash_dev at32_onchip_flash = { "onchip_flash", AT32_FLASH_START_ADRESS, AT32_FLASH_SIZE, AT32_FLASH_PAGE_SIZE, {NULL, fal_flash_read, fal_flash_write, fal_flash_erase} };
+ 
+ static int fal_flash_read(long offset, rt_uint8_t *buf, size_t size)
+ {
+     return at32_flash_read(at32_onchip_flash.addr + offset, buf, size);
+ }
+ 
+ static int fal_flash_write(long offset, const rt_uint8_t *buf, size_t size)
+ {
+     return at32_flash_write(at32_onchip_flash.addr + offset, buf, size);
+ }
+ 
+ static int fal_flash_erase(long offset, size_t size)
+ {
+     return at32_flash_erase(at32_onchip_flash.addr + offset, size);
+ }
+ 
+#endif
+#endif /* BSP_USING_ON_CHIP_FLASH */
+
+
